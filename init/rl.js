8acdf4bbf1a7eb096e4975d82a37bec7f33d4fe3
FNH99 <148479741+FNH99@users.noreply.github.com>
2024-05-12 09:45:08 +0700

    initialize the map structure

--- init/rl.js
+++ init/rl.js
@@ -17,6 +17,8 @@ var game = new Phaser.Game(COLS * FONT * 0.6, ROWS * FONT, Phaser.AUTO, null, {
 function create() {
     // init keyboard commands
     game.input.keyboard.addCallbacks(null, null, onKeyUp);
+    // initialize map
+    initMap();
 }
 
 function onKeyUp(event) {
@@ -25,5 +27,23 @@ function onKeyUp(event) {
         case Keyboard.RIGHT:
         case Keyboard.UP:
         case Keyboard.DOWN:
+    }
+}
+
+// structure of the map
+var map;
+
+function initMap() {
+    // create a new random map
+    map = [];
+    for (var y = 0; y < ROWS; y++) {
+        var newRow = [];
+        for (var x = 0; x < COLS; x++) {
+            if (Math.random() > 0.8)
+                newRow.push('#');
+            else
+                newRow.push('.');
+        }
+        map.push(newRow);
     }
 }
